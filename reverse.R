a0d83d753fd27b04828ce4f302e5db80064706dd
Jae Yeon Hwang <wodus0515@gmail.com>
2017-08-08 10:31:30 -0600

    Restoring reverse.R file.

---
+++ reverse.R
@@ -0,0 +1,15 @@
+#' Reverse
+#' 
+#' Reverse returns a copy of a vector whose elements are in
+#' the reverse order. The end 
+#' 
+#' @param x
+#'   
+#' @return a vector
+#' @export
+#' 
+#' @examples
+#' reverse(1:10)
+reverse <- function(x) {
+  if (length(x) > 0) x[length(x):1] else x
+}
